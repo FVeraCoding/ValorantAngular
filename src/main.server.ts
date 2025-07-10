85d6baac141eb6d4dc1c237ba68e9ff4bebc65a9
Fernando Vera <fveracoding@gmail.com>
2025-07-10 18:03:04 +0200

    actualizados tsconfig.server.json y main.server.ts

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,1 +1,8 @@
-export { AppServerModule as default } from './app/app.module.server';
+import { CommonEngine } from '@angular/ssr/node';
+import { render } from '@netlify/angular-runtime/common-engine';
+
+const commonEngine = new CommonEngine();
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  return await render(commonEngine);
+}
